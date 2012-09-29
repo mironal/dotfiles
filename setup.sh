0df7d300db033253aa534bfa425fea1b838428b0
mironal <miro.gunp@gmail.com>
2012-09-29 09:35:17 +0900

    zsh用にした

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.bashrc .git-completion.bash .git-prompt.sh .gitconfig .tmux.conf .vimrc .vim)
+DOT_FILES=(.gitconfig .tmux.conf .vimrc .vim .zshrc)
 for file in ${DOT_FILES[@]}
 do
   ln -i -s $HOME/dotfiles/$file $HOME/$file
