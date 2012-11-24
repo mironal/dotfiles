3ae80378c4015bb6bd02681300748d46af6fc859
mironal <miro.gunp@gmail.com>
2012-11-24 23:44:52 +0900

    setup.shを合わせた

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-DOT_FILES=(.gitconfig .tmux.conf .vimrc .vim .zshrc)
+DOT_FILES=(.gitconfig .tmux.conf .vimrc .vim .zshrc .oh-my-zsh)
 for file in ${DOT_FILES[@]}
 do
   ln -i -s $HOME/dotfiles/$file $HOME/$file
 done
+
+touch $HOME/.my_zshrc
+
