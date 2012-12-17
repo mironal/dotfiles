7455815d8573ab1c968582aa11e9bd5ec7a684c6
mironal <miro.gunp@gmail.com>
2012-12-17 20:40:54 +0900

    修正してやる！！！

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.gitconfig .tmux.conf .vimrc .vim .zshrc .oh-my-zsh)
+DOT_FILES=(.gitconfig .tmux.conf .vimrc .zshrc .oh-my-zsh)
 for file in ${DOT_FILES[@]}
 do
   ln -i -s $HOME/dotfiles/$file $HOME/$file
@@ -6,16 +6,15 @@ done
 
 touch $HOME/.my_zshrc
 
-
-cp bin/tmuxx $HOME/bin/
-cp bin/reattach-to-user-namespace $HOME/bin
-
 cp -r .vim ~/.vim
-
 
 case "${OSTYPE}" in
 freebsd*|darwin*)
-
+    if [ ! -e ~/bin ];then
+        mkdir ~/bin
+    fi
+    cp bin/tmuxx $HOME/bin/
+    cp bin/reattach-to-user-namespace $HOME/bin
     echo "alias tmux='tmuxx'" >> ~/.bashrc
     echo "alias tm='tmuxx'" >> ~/.bashrc
     echo "alias tma='tmux attach'" >> ~/.bashrc
