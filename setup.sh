1d6f27756de1171eea7cbe5f1158cbe6da5ce7a4
mironal <miro.gunp@gmail.com>
2013-03-06 09:20:53 +0900

    .ctagsもシンボリックリンクするようにした.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.gitconfig .tmux.conf .vimrc .zshrc .oh-my-zsh)
+DOT_FILES=(.gitconfig .tmux.conf .vimrc .zshrc .oh-my-zsh .ctags)
 for file in ${DOT_FILES[@]}
 do
   ln -i -s $HOME/dotfiles/$file $HOME/$file
