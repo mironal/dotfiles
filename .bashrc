eacaf16ee0e7a911cb2601cc28245c80f76f0d76
mironal <miro.gunp@gmail.com>
2013-06-03 07:13:22 +0900

    tmuxxをやめた

--- .bashrc
+++ .bashrc
@@ -1,9 +1,3 @@
 PATH=$PATH:$HOME/bin
 export PATH
 
-# tmux
-alias tmux='tmuxx'
-alias tm='tmuxx'
-alias tma='tmux attach'
-alias tml='tmux list-window'
-
--- setup.sh
+++ setup.sh
@@ -4,19 +4,3 @@ do
   ln -i -s $HOME/dotfiles/$file $HOME/$file
 done
 
-touch $HOME/.my_zshrc
-
-case "${OSTYPE}" in
-freebsd*|darwin*)
-    if [ ! -e ~/bin ];then
-        mkdir ~/bin
-    fi
-    cp bin/tmuxx $HOME/bin/
-    cp bin/reattach-to-user-namespace $HOME/bin
-    echo "alias tmux='tmuxx'" >> ~/.bashrc
-    echo "alias tm='tmuxx'" >> ~/.bashrc
-    echo "alias tma='tmux attach'" >> ~/.bashrc
-    echo "alias tml='tmux list-window'" >> ~/.bashrc
-  ;;
-esac
-
